27cd59c90d26a1dbd97a849b17d74d9fd8d73908
Chris <zyy1120126@gmail.com>
2020-08-26 23:30:10 +0800

    20200826 commit

--- taiwanize.py
+++ taiwanize.py
@@ -1,3 +1,4 @@
+#import cn2tw
 from pptx import Presentation
 from langconv import *
 
@@ -9,39 +10,21 @@ def Traditional2Simplified(sentence): #繁體轉簡體
 def Simplified2Traditional(sentence): #簡體轉繁體
     sentence = str(sentence)
     sentence = Converter('zh-hant').convert(sentence)
+    #sentence = Converter('zh-hant').convert(sentence)
     return sentence
 
 def run( ): 
-    ppt = Presentation("example.pptx")
-    for slide in ppt.slides:    #檢視每個頁面
-	    for shape in slide.shapes:    #檢視每個文字框
-		    if shape.has_text_frame:  #判斷該shape內是否有的文字
-			    text_frame = shape.text   #獲取文字框
-                TEST2 = Simplified2Traditional(TEST1)    #簡體轉繁體繁體還是繁體
-
-
-
-
-			    print(TEST2) 
-
-                
-
-                
-
-    print("幹")
-
-    
+    prs = Presentation("example.pptx")
+    for slide in prs.slides:            #檢視每個頁面
+        for shape in slide.shapes:      #檢視每個框框
+            if shape.has_text_frame:    #判斷該shape內是否有的文字
+                text_frame = shape.text_frame  #獲取文字框
+                TEST2 = Simplified2Traditional(text_frame.text) #文字繁體轉簡體
+                print(TEST2)
+                cur_text = text_frame.paragraphs[0].runs[0].text
+                new_text = cur_text.replace(str(text_frame.text), str(TEST2)) #取代
+                text_frame.paragraphs[0].runs[0].text = new_text #寫回
+    prs.save("example2.pptx")
+ 
 if __name__=="__main__":
     run( )
-
-                
-
-
-
-
-                
-
-                
-            
-
-    
--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 from pptx import Presentation
+from langconv import *
 
-search_str = "old text"
-repl_str = "123"
+def Traditional2Simplified(sentence): #繁體轉簡體
+    sentence = str(sentence)
+    sentence = Converter('zh-hans').convert(sentence)
+    return sentence
 
-prs = Presentation("example.pptx")
-for slide in prs.slides:
-    for shape in slide.shapes:
-        if shape.has_text_frame:
-            if(shape.text.find(search_str))!=-1:
-                text_frame = shape.text_frame
+def Simplified2Traditional(sentence): #簡體轉繁體
+    sentence = str(sentence)
+    sentence = Converter('zh-hant').convert(sentence)
+    return sentence
+
+def run( ): 
+    prs = Presentation("example.pptx")
+    for slide in prs.slides:            #檢視每個頁面
+        for shape in slide.shapes:      #檢視每個框框
+            if shape.has_text_frame:    #判斷該shape內是否有的文字
+                text_frame = shape.text_frame  #獲取文字框
+                TEST2 = Simplified2Traditional(text_frame.text) #文字繁體轉簡體
+                print(TEST2)
                 cur_text = text_frame.paragraphs[0].runs[0].text
-                new_text = cur_text.replace(str(search_str), str(repl_str))
-                text_frame.paragraphs[0].runs[0].text = new_text
-prs.save("example2.pptx")
-
-
-    
+                new_text = cur_text.replace(str(text_frame.text), str(TEST2)) #取代
+                text_frame.paragraphs[0].runs[0].text = new_text #寫回
+    prs.save("example2.pptx")
+ 
+if __name__=="__main__":
+    run( )
