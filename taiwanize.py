cb5326bce948787827f4c22c4a6f5bfdc625f3a6
Johnny Yeng <a2902793@gmail.com>
2021-08-03 22:11:20 +0800

    Refactor code

--- taiwanize.py
+++ taiwanize.py
@@ -1,30 +1,52 @@
-#import cn2tw
+#!/usr/bin/env python
 from pptx import Presentation
-from langconv import *
+from langconv import Converter
+from pathlib import Path
+import argparse, sys
 
-def Traditional2Simplified(sentence): #繁體轉簡體
-    sentence = str(sentence)
-    sentence = Converter('zh-hans').convert(sentence)
+def Traditional2Simplified(sentence):  # 繁體轉簡體
+    sentence = Converter('zh-hans').convert(str(sentence))
     return sentence
 
-def Simplified2Traditional(sentence): #簡體轉繁體
-    sentence = str(sentence)
-    sentence = Converter('zh-hant').convert(sentence)
-    #sentence = Converter('zh-hant').convert(sentence)
+
+def Simplified2Traditional(sentence):  # 簡體轉繁體
+    sentence = Converter('zh-hant').convert(str(sentence))
     return sentence
 
-def run( ): 
-    prs = Presentation("example.pptx")
-    for slide in prs.slides:            #檢視每個頁面
-        for shape in slide.shapes:      #檢視每個框框
-            if shape.has_text_frame:    #判斷該shape內是否有的文字
-                text_frame = shape.text_frame  #獲取文字框
-                TEST2 = Simplified2Traditional(text_frame.text) #文字繁體轉簡體
-                print(TEST2)
-                cur_text = text_frame.paragraphs[0].runs[0].text
-                new_text = cur_text.replace(str(text_frame.text), str(TEST2)) #取代
-                text_frame.paragraphs[0].runs[0].text = new_text #寫回
-    prs.save("example2.pptx")
- 
-if __name__=="__main__":
-    run( )
+def PowerPoint(filepath):
+    prs = Presentation(filepath)
+    for slide in prs.slides:                                         # 每張投影片
+        for shape in slide.shapes:                                   # 每張投影片/每個物件
+            if shape.has_text_frame:                                 # 判斷每張投影片/每個物件是否包含文字框
+                for paragraph in shape.text_frame.paragraphs:        # 每張投影片/每個物件/每個文字段落
+                    for run in paragraph.runs:                       # 每張投影片/每個物件/每個文字段落/每個文字流
+                        run.text = Simplified2Traditional(run.text)  # 將每張投影片/每個物件/每個文字段落/每個文字流的簡體中文翻成繁體中文
+                        """
+                        不可直接用 text_frame.text 的原因是因為「格式會跑掉」
+                        從層級上來分，slide > shape > text_frame > paragraph > run
+                                     投影片   物件      文字框      文字段落   文字流
+                        
+                        * 間距(line_spacing)是在 paragraph 中設定
+                        * 字體(font)、大小(size)、粗斜體跟顏色等在最小層級的 run 中設定
+
+                        若是直接使用層級較大的 text_frame.text 換詞語，等於 paragraph.line_spacing、run.font、run.size 等等的參數未設定，
+                        這樣會導致原本的格式被刷掉。若要維持一樣的格式要指定只換掉 run.text 這樣其餘沒被你動到格式都會保留。
+                        """
+    prs.save("example_TW.pptx")
+
+def main():
+    parser = argparse.ArgumentParser(description='【台灣化（Taiwanized），一個將中國用詞用語翻成台灣化的 Python 腳本。】')
+    parser.add_argument('filename', metavar='F', nargs='+', help='檔案名稱，範例："python taiwanized.py example.pptx"')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    args = parser.parse_args()
+    for file in args.filename:
+        if Path(file).suffix == '.pptx':
+            PowerPoint(file)
+        else:
+            print("目前不支援檔案: " + '"' + file + '"')
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,28 +1,12 @@
-from pptx import Presentation
-from langconv import *
+import argparse
 
-def Traditional2Simplified(sentence): #繁體轉簡體
-    sentence = str(sentence)
-    sentence = Converter('zh-hans').convert(sentence)
-    return sentence
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--ppt', '--pptx',
+                    action='store_true', help='verbose flag')
 
-def Simplified2Traditional(sentence): #簡體轉繁體
-    sentence = str(sentence)
-    sentence = Converter('zh-hant').convert(sentence)
-    return sentence
+args = parser.parse_args()
 
-def run( ): 
-    prs = Presentation("example.pptx")
-    for slide in prs.slides:            #檢視每個頁面
-        for shape in slide.shapes:      #檢視每個框框
-            if shape.has_text_frame:    #判斷該shape內是否有的文字
-                text_frame = shape.text_frame  #獲取文字框
-                TEST2 = Simplified2Traditional(text_frame.text) #文字繁體轉簡體
-                print(TEST2)
-                cur_text = text_frame.paragraphs[0].runs[0].text
-                new_text = cur_text.replace(str(text_frame.text), str(TEST2)) #取代
-                text_frame.paragraphs[0].runs[0].text = new_text #寫回
-    prs.save("example2.pptx")
- 
-if __name__=="__main__":
-    run( )
+if args.ppt:
+    print("~ Verbose!")
+else:
+    print("~ Not so verbose")
